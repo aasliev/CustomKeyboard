2fb77a3ffefaaaaf18a4cba1a8214f8d9673cfe6
Asliddin Asliev <asliev.asliddin@gmail.com>
2020-09-28 14:51:19 -0400

    Initial Commit

---
+++ Customkeyboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Customkeyboard
+//
+//  Created by Asliddin Asliev on 9/28/20.
+//  Copyright © 2020 Asliddin Asliev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
